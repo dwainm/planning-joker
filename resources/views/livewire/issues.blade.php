3d2a7dd93501416d7f60f00dfb20d7a9f3c84ddc
Dwain Maralack <dwain.maralack@gmail.com>
2023-05-26 13:54:22 +0200

    Remove issue.blade to simplify template.

--- resources/views/livewire/issues.blade.php
+++ resources/views/livewire/issues.blade.php
@@ -9,7 +9,33 @@
 
 		<ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700 space-x-9">
 		@foreach ($issues as $issue)
-			@livewire('issue', ['issue'=>$issue, 'votes'=>$votes])
+            <li class="pb-0">
+              <div class="flex items-center space-x-30">
+                 <div class="flex-shrink-0 mb-3">
+                 {{--
+                    <img class="mr-20 w-8 h-8 rounded-full" src="{{ asset('images/github_black_logo_icon.png') }}" alt="Github logo">
+                    --}}
+                 </div>
+                 <div class="flex-1 min-w-0">
+                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        {{ $issue['github_issue_title'] }}
+                    </p>
+                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                    Current Estimate {{ $issue['github_issue_estimate'] }}
+                    @if ( ! empty( $issue['github_url'] ) )
+                        | <a target="_blank" href="{{$issue['github_url']}}">Link</a> |
+                    @endif Description: {!! $issue['github_issue_description'] !!}
+                    </p>
+                 </div>
+              </div>
+                    <div>
+                         @foreach( [1,2,3,5,8,13,21] as $option )
+                            {{ $issue_vote = $votes[$issue['id']]['estimate']??''}}
+                            <input @checked($issue_vote==$option) type="radio" id="{{$issue['id']}}-estimate-{{$option}}" name="estimate[{{$issue['id']}}]" value="{{$option}}">
+                            <label for="{{$issue['id']}}-estimate-{{$option}}">{{$option}}</label>
+                        @endforeach
+                    </div>
+            </li>
 			<hr class="w-ful h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700">
 		@endforeach
 		</ul>
